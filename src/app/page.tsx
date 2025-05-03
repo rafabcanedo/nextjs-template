58a7b63d67cc4de77015ed3e3eac332d8e1fddbb
rafabcanedo <rafaitecanedo@gmail.com>
2025-05-03 17:24:56 -0300

    add test button and input in home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,16 @@
+import { Button } from '@/components/button';
+import Input from '@/components/input';
+
 export default function Home() {
   return (
-    <div>
+    <div className="min-h-screen flex flex-col items-center justify-center">
       <h1>CanedoDev</h1>
+      <div>
+        <Input />
+      </div>
+      <div>
+        <Button>Test</Button>
+      </div>
     </div>
   );
 }
